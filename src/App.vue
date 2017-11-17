0a16c617195cec743ecd5206d8046b00b826bf25
Bryan <bryan@unknown809B20B82DE8.attlocal.net>
2017-11-17 06:34:49 -0600

    adding vue-router to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <!-- the router outlet, where all matched components would be viewed -->
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
 }
 </script>
-
+<!-- styling for the component -->
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -mox-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
